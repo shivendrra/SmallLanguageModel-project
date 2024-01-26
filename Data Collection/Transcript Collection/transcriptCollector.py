09abdb65b1788b3dc1a07d5ccc571109657e63ed
Shivendra <shivharsh44@gmail.com>
2024-01-27 01:25:44 +0530

    added progress visualization and summary

--- Data Collection/Transcript Collection/transcriptCollector.py
+++ Data Collection/Transcript Collection/transcriptCollector.py
@@ -5,6 +5,7 @@ from googleapiclient.discovery import build
 from youtube_transcript_api import TranscriptsDisabled, YouTubeTranscriptApi
 import logging
 import timeit
+from tqdm import tqdm  # Import tqdm
 
 start_time = timeit.default_timer()
 logging.basicConfig(filename='youtube_fetch.log', level=logging.ERROR)
@@ -14,64 +15,62 @@ api_key = os.getenv('yt_secret_key')
 os.chdir("d:/Machine Learning/SLM-Project/")
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-file_path = 'Data Collection/channel_ids_snippet.json'
+file_path = 'Data Collection/Transcript Collection/channel_ids_snippet.json'
 with open(file_path, 'r') as file:
   channelData = json.load(file)
 
-print('code start!')
-
 videoNo = 0
 for links in channelData:
-  next_page_token = None
-  videoIds = []
+    next_page_token = None
+    videoIds = []
 
-  while True:
-    channelRes = youtube.channels().list(
-      part='contentDetails', id=links
-    ).execute()
+    while True:
+        channelRes = youtube.channels().list(
+            part='contentDetails,snippet', id=links
+        ).execute()
+        if 'items' in channelRes and channelRes['items']:
+            channel_name = channelRes["items"][0]["snippet"]["title"]
+            playlistId = channelRes['items'][0]['contentDetails']['relatedPlaylists']['uploads']
 
-    if 'items' in channelRes and channelRes['items']:
-      playlistId = channelRes['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+            playlistResult = youtube.playlistItems().list(
+                part='contentDetails', playlistId=playlistId,
+                maxResults=100, pageToken=next_page_token
+            ).execute()
 
-      playlistResult = youtube.playlistItems().list(
-        part='contentDetails', playlistId=playlistId,
-        maxResults = 100, pageToken = next_page_token
-      ).execute()
+            videoIds.extend([item['contentDetails']['videoId'] for item in playlistResult.get('items', [])])
 
-      videoIds.extend([item['contentDetails']['videoId'] for item in playlistResult.get('items', [])])
+            next_page_token = playlistResult.get('nextPageToken')
 
-      next_page_token = playlistResult.get('nextPageToken')
+            if not next_page_token:
+                break
 
-      if not next_page_token:
-        break
-  
-  for ids in videoIds:
-    videoUrl = f"https://www.youtube.com/watch?v={ids}"
-    try:
-      raw_transcripts = []
-      try:
-        captions = YouTubeTranscriptApi.get_transcript(
-          ids, languages=['en'], preserve_formatting=True
-        )
-        if captions:
-          formatted_captions = [{'text': caption['text']} for caption in captions]
-          raw_transcripts.append(formatted_captions)
-          videoNo += 1
-          if videoNo % 500 == 0:
-            print('\n', f"Number of videos with valid captions are: {videoNo}", '\n')
-        else:
-          continue
-      except TranscriptsDisabled as e:
-        print("subtitles not present!")
-      except Exception as e:
-        logging.error(f"There was some error while fetching the video: {str(e)}")
-    except Exception as e:
-      logging.error(f"There was some error while getting the captions: {str(e)}")
+    with tqdm(total=len(videoIds), desc=f"Fetching '{channel_name}' videos") as pbar:
+        for ids in videoIds:
+            videoUrl = f"https://www.youtube.com/watch?v={ids}"
+            try:
+                raw_transcripts = []
+                try:
+                    captions = YouTubeTranscriptApi.get_transcript(
+                        ids, languages=['en'], preserve_formatting=True
+                    )
+                    if captions:
+                        formatted_captions = [{'text': caption['text']} for caption in captions]
+                        raw_transcripts.append(formatted_captions)
+                        videoNo += 1
+                        pbar.update(1)
+                    else:
+                        continue
+                except TranscriptsDisabled as e:
+                    continue
+                except Exception as e:
+                    logging.error(f"There was some error while fetching the video: {str(e)}")
+            except Exception as e:
+                logging.error(f"There was some error while getting the captions: {str(e)}")
 
-    with open('Data/txt files/caption_data.txt', 'a', encoding='utf-8') as file:
-      for videoCaptions in raw_transcripts:
-        for line in videoCaptions:
-          file.write(line['text'] + ' ')
+            with open('Data/caption files/new_data.txt', 'a', encoding='utf-8') as file:
+                for videoCaptions in raw_transcripts:
+                    for line in videoCaptions:
+                        file.write(line['text'] + ' ')
 
 print(f"total {videoNo} videos were fetched")
 print(f"time taken to execute the code is {(timeit.default_timer() - start_time) / 3600} hrs")
