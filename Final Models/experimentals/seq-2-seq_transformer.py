15a7035432a2aee3582670c720ab3b605a8910ff
Shivendra <shivharsh44@gmail.com>
2024-03-13 00:59:26 +0530

    new model architecture added

--- Final Models/experimentals/seq-2-seq_transformer.py
+++ Final Models/experimentals/seq-2-seq_transformer.py
@@ -1,51 +1,30 @@
-"""
-  -> this is a proper seq-2-seq transformer model with encoder and decoder layers
-  -> this is a more complex one, just for understanding
-"""
-
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 torch.manual_seed(1400)
 
 class Head(nn.Module):
-  """ one head of self attention """
-
-  def __init__(self, n_embd, n_head, dropout, block_size):
-    head_size = n_embd // n_head
+  def __init__(self, d_embd, n_head, dropout, block_size):
+    head_size = d_embd // n_head
     super().__init__()
-    self.key = nn.Linear(n_embd, head_size, bias=False)
-    self.query = nn.Linear(n_embd, head_size, bias=False)
-    self.value = nn.Linear(n_embd, head_size, bias=False)
+    self.key = nn.Linear(d_embd, head_size, bias=True)
+    self.query = nn.Linear(d_embd, head_size, bias=True)
+    self.value = nn.Linear(d_embd, head_size, bias=True)
     self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
     self.dropout = nn.Dropout(dropout)
-
+  
   def forward(self, x):
-    B, T, C = x.shape
-    k = self.key(x)
-    q = self.query(x)
-  
-    # compute attention scores ("affinities")
-    wei = q @ k.transpose(-2, -1) * (C // self.tril.size(-1))**-0.5
-    wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
-    wei = F.softmax(wei, dim=-1)
-    wei = self.dropout(wei)
-  
-    # perform the weighted aggregation of the values
-    v = self.value(x)
-    out = wei @ v
-    return out
+    pass
 
 class MultiHeadAttention(nn.Module):
-  """ Multiple heads of self-attention in parallel"""
-
-  def __init__(self, n_embd, n_head, dropout, block_size):
+  def __init__(self, d_embd, n_head, dropout, block_size):
     super().__init__()
-    self.heads = nn.ModuleList([Head(n_embd, n_head, dropout, block_size) for _ in range(n_head)])
-    self.proj = nn.Linear(n_head * (n_embd // n_head), n_embd)
+    self.heads = nn.ModuleList([Head(d_embd=d_embd, n_head=n_head, dropout=dropout, block_size=block_size) for _ in range(n_head)])
+    self.proj = nn.Linear(n_head * (d_embd // n_head), d_embd)
     self.dropout = nn.Dropout(dropout)
-
+  
   def forward(self, x):
     out = torch.cat([h(x) for h in self.heads], dim=-1)
     out = self.dropout(out)
@@ -53,75 +32,103 @@ class MultiHeadAttention(nn.Module):
     return out
 
 class FeedForward:
-  def __init__(self, n_embd):
-    super(FeedForward, self).__init__()
-    self.fc1 = nn.Linear(n_embd, 4*n_embd)  ## n_ff = 4*n_embd
-    self.fc2 = nn.Linear(4*n_embd, n_embd)
+  def __init__(self, d_embd):
+    super().__init__()
+    self.fc1 = nn.Linear(d_embd, 4*d_embd)  # n_ff = 4*d_embd
+    self.fc2 = nn.Linear(4*d_embd, d_embd)
   
   def forward(self, x):
     x = F.gelu(self.fc1(x))
     x = self.fc2(x)
     return x
+  
+class EncoderDecoderAttention(nn.Module):
+  def __init__(self, d_embd, n_head, dropout, block_size):
+    super().__init__()
+    self.heads = nn.ModuleList([Head(d_embd, n_head, dropout, block_size) for _ in range(n_head)])
+    self.proj = nn.Linear(n_head * (d_embd // n_head), d_embd)
+    self.dropout = nn.Dropout(dropout)
+
+  def forward(self, query, key, value, mask=None):
+    x = torch.cat((key, query, value), dim=-1)
+    energies = []
+    for head in self.heads:
+        energy = head(x)
+        energies.append(energy.unsqueeze(1))
+    energy = torch.cat(energies, dim=1)
+    energy = self.proj(energy)
+    energy = self.dropout(energy)
+
+    if mask is not None:
+      energy = energy.masked_fill(mask == 0, float('-inf'))
+
+    attention = F.softmax(energy, dim=-1)
+    output = torch.matmul(attention, value)
+
+    return output
 
 class EncoderLayer(nn.Module):
-  def __init__(self, n_embd, n_head, n_ff, dropout):
-    super(EncoderLayer, self).__init__()
-    self.self_attention = MultiHeadAttention(n_embd, n_head)
-    self.feed_forward = FeedForward(n_embd, n_ff)
+  def __init__(self, d_embd, n_head, dropout, block_size):
+    super().__init__()
+    self.s_att = MultiHeadAttention(d_embd=d_embd, n_head=n_head, block_size=block_size, dropout=dropout)
+    self.ffwd = FeedForward(d_embd=d_embd)
     self.dropout = nn.Dropout(dropout)
-    self.norm1 = nn.LayerNorm(n_embd)
-    self.norm2 = nn.LayerNorm(n_embd)
+    self.norm1 = nn.LayerNorm(d_embd)
+    self.norm2 = nn.LayerNorm(d_embd)
   
-  def forward(self, src, src_mask):
-    src2 = self.self_attention(src, src, src, src_mask)
+  def forward(self, src, src_mask=None):
+    src2 = self.s_att(src)
     src = src + self.dropout(src2)
     src = self.norm1(src)
 
-    src2 = self.feed_forward(src)
+    src2 = self.ffwd(src)
     src = src + self.dropout(src2)
     src = self.norm2(src)
-    
+  
     return src
 
 class DecoderLayer(nn.Module):
-  def __init__(self, n_embd, n_head, n_ff, dropout):
-    super(DecoderLayer, self).__init__()
-    self.self_attention = MultiHeadAttention(n_embd, n_head)
-    self.encoder_attention = MultiHeadAttention(n_embd, n_head)
-    self.feed_forward = FeedForward(n_embd, n_ff)
+  def __init__(self, d_embd, n_head, dropout, block_size) -> None:
+    super().__init__()
+    self.s_att = MultiHeadAttention(d_embd=d_embd, n_head=n_head, block_size=block_size, dropout=dropout)
+    self.enc_att = EncoderDecoderAttention(d_embd=d_embd, n_head=n_head, block_size=block_size, dropout=dropout)
+    self.ffwd = FeedForward(d_embd=d_embd)
     self.dropout = nn.Dropout(dropout)
-    self.norm1 = nn.LayerNorm(n_embd)
-    self.norm2 = nn.LayerNorm(n_embd)
-    self.norm3 = nn.LayerNorm(n_embd)
+    self.norm1 = nn.LayerNorm(d_embd)
+    self.norm2 = nn.LayerNorm(d_embd)
+    self.norm3 = nn.LayerNorm(d_embd)
   
-  def forward(self, trg, enc_src, trg_mask, src_mask):
-    trg2 = self.self_attention(trg, trg, trg, trg_mask)
-    trg = trg + self.dropout(trg2)
+  def forward(self, trg, enc_src, trg_mask=None, src_mask=None):
+    trg2 = self.s_att(trg)
+    trg = trg2 + self.dropout(trg2)
     trg = self.norm1(trg)
 
-    trg2 = self.encoder_attention(trg, enc_src, enc_src, src_mask)
+    trg2 = self.enc_att(trg, enc_src, enc_src)
     trg = trg + self.dropout(trg2)
     trg = self.norm2(trg)
 
-    trg2 = self.feed_forward(trg)
+    trg2 = self.ffwd(trg)
     trg = trg + self.dropout(trg2)
     trg = self.norm3(trg)
-
+  
     return trg
 
-class TransformerModel(nn.Module):
-  def __init__(self, n_embd, n_ff, n_head, n_layers, dropout, vocab_size, block_size, batch_size, src_vocab_size, trg_vocab_size,):
+class Transformer(nn.Module):
+  def __init__(self, block_size, vocab_size, n_layers, d_embd, n_head, dropout):
     super().__init__()
-    self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
-    self.position_embedding_table = nn.Embedding(block_size, n_embd)
-    self.encoder_layers = nn.ModuleList([EncoderLayer(n_embd, n_head, n_ff, dropout) for _ in range(n_layers)])
-    self.decoder_layers = nn.ModuleList([DecoderLayer(n_embd, n_head, n_ff, dropout) for _ in range(n_layers)])
-    self.ln_f = nn.LayerNorm(n_embd)
-    self.lm_head = nn.Linear(n_embd, vocab_size)
-    self.fc_out = nn.Linear(n_embd, vocab_size)
+    self.d_embd = d_embd
+    self.block_size = block_size
+    
+    self.token_embd = nn.Embedding(vocab_size, d_embd)
+    self.pos_embd = nn.Embedding(block_size, d_embd)
+    self.enc_layer = nn.ModuleList([EncoderLayer(n_head=n_head, block_size=block_size, dropout=dropout, d_embd=d_embd) for _ in range(n_layers)])
+    self.dec_layer = nn.ModuleList([DecoderLayer(n_head=n_head, block_size=block_size, dropout=dropout, d_embd=d_embd) for _ in range(n_layers)])
+    
+    self.norm_final = nn.LayerNorm(d_embd)
+    self.lm_head = nn.Linear(d_embd, vocab_size)
+    self.fc_out = nn.Linear(d_embd, vocab_size)
     self.dropout = nn.Dropout(dropout)
     self.apply(self._init_weights)
-    self.n_embd = n_embd
 
   def _init_weights(self, module):
     if isinstance(module, nn.Linear):
@@ -145,30 +152,30 @@ class TransformerModel(nn.Module):
   def forward(self, idx, targets=None):
     B, T = idx.shape
 
-    tok_embd = self.token_embedding_table(idx)
-    pos_embd = self.position_embedding_table(torch.arange(T, device=device))
+    tok_embd = self.token_embd(idx)
+    pos_embd = self.pos_embd(torch.arange(T, device=device))
     x = tok_embd + pos_embd
 
-    for layer in self.encoder_layers:
+    for layer in self.enc_layer:
       x = layer(x, None)
         
-    for layer in self.decoder_layers:
+    for layer in self.dec_layer:
       x = layer(x, x)
     
-    x = self.ln_f(x)
+    x = self.norm_final(x)
     logits = self.lm_head(x)
 
     if targets is None:
       loss = None
     
     else:
-      B, T, C = logits.shape()
+      B, T, C = logits.shape
       logits = logits.view(B*T, C)
       targets = targets.view(B*T)
       loss = F.cross_entropy(logits, targets, ignore_index=-52, reduction='mean')
     
     return logits, loss
-
+  
   def generate(self, idx, max_tokens=50):
     for _ in range(max_tokens):
       idx_cond = idx[:, -self.block_size: ]
